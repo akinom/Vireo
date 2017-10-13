bf2d83e7abea3e17c5c60677c20dc5982c940492
Monika Mevenkamp <monikam@princeton.edu>
2017-10-13 19:04:23 +0000

    aws - startup vireo under ec2-user; add .cshrc ... to setup ec2-user

--- aws/vireo-init.sh
+++ aws/vireo-init.sh
@@ -13,12 +13,17 @@ PLAY=/home/ec2-user/play1/play
 
 case "$1" in
     start)
+	sudo -u $VIREOUSER $VIREOHOME/aws/vireo.sh $PLAY $1
+        ;;
     stop)
+	sudo -u $VIREOUSER $VIREOHOME/aws/vireo.sh $PLAY $1
+        ;;
     status)
-		sudo -u $VIREOUSER $VIREOHOME/aws/vireo $PLAY $1
+	sudo -u $VIREOUSER $VIREOHOME/aws/vireo.sh $PLAY $1
+        ;;
     restart)
-		sudo -u $VIREOUSER $VIREOHOME/aws/vireo $PLAY  stop
-		sudo -u $VIREOUSER $VIREOHOME/aws/vireo $PLAY start
+	sudo -u $VIREOUSER $VIREOHOME/aws/vireo.sh $PLAY stop
+	sudo -u $VIREOUSER $VIREOHOME/aws/vireo.sh $PLAY start
         ;;
     *)
         echo "Usage:  {start|stop|status|restart"
