f811579f2df2d0bc789520e53358cd373bbe1472
Monika Mevenkamp <monikam@princeton.edu>
2017-10-16 10:25:27 -0400

    also send email with list of included issues

--- aws/vireo.sh
+++ aws/vireo.sh
@@ -10,6 +10,7 @@ then
    git stash;
    git pull;
    git stash apply;
+   git log --merges --author Mevenkamp | fgrep issue | mail -s 'Vireo- issue list' monikam@princeton.edu ldurgin@princeton.edu
 fi 
 
 cd ..
