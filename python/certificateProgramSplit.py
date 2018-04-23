b6d2df572bc7d6b5b14561cee78e25ddd55718f6
Monika Mevenkamp <monikam@princeton.edu>
2018-04-23 14:46:48 -0400

    rename fields

--- python/certificateProgramSplit.py
+++ python/certificateProgramSplit.py
@@ -24,37 +24,37 @@ class ArgParser(argparse.ArgumentParser):
 
 class Vireo:
     def __init__(self, args):
-        self.filename = args.excel
-        self.wb = openpyxl.load_workbook(filename = args.excel)
-        if (1 != len(self.wb.worksheets)) :
-            raise Exception("Workbook '%s' should have exactly one sheet" % (args.excel))
-        self.sheet = self.wb.worksheets[0]
+        self.thesis_file = args.thesis
+        self.thesis_wb = openpyxl.load_workbook(filename = args.thesis)
+        if (1 != len(self.thesis_wb.worksheets)) :
+            raise Exception("Workbook '%s' should have exactly one sheet" % (args.thesis_excel))
+        self.thesis = self.thesis_wb.worksheets[0]
         self.split_col_name = 'Certificate Program'
         self.split_col = self._col_index_of(self.split_col_name)
         if (None == self.split_col):
-            raise Exception("Workbook '%s' does not contain a '%s' column" % (args.excel, self.split_col_name))
+            raise Exception("Workbook '%s' does not contain a '%s' column" % (args.thesis_excel, self.split_col_name))
         self.id_col_name = 'ID'
         self.id_col = self._col_index_of(self.id_col_name)
         if (None == self.id_col):
-            raise Exception("Workbook '%s' does not contain a '%s' column" % (args.excel, self.id_col_name))
+            raise Exception("Workbook '%s' does not contain a '%s' column" % (args.thesis_excel, self.id_col_name))
         self.id_rows = {}
         self._id_rows()
 
     def print_info(self):
-        print("filename %s" % self.filename)
-        print("workbook %s" % self.sheet.title)
+        print("filename %s" % self.thesis_file)
+        print("workbook %s" % self.thesis.title)
         print("id column: %s (%d)" % (self.id_col_name,  self.id_col))
         print("splitting on value in column: %s (%d)" % (self.split_col_name,  self.split_col))
 
     def _col_index_of(self, title):
-        for row in self.sheet.iter_rows(min_row=1, max_row=1):
+        for row in self.thesis.iter_rows(min_row=1, max_row=1):
             for cell in  row:
                 if (cell.value == title):
                     return cell.col_idx -1;
         return None;
 
     def _dump_sheet(self):
-        for row in self.sheet.iter_rows(min_row=1, max_row=1):
+        for row in self.thesis.iter_rows(min_row=1, max_row=1):
             for cell in  row:
                 print("%s" % (cell.value))
             print("---")
@@ -62,7 +62,7 @@ class Vireo:
 
     def split_sheet(self):
         splits = {};
-        for row in self.sheet.iter_rows(min_row=2):
+        for row in self.thesis.iter_rows(min_row=2):
             col_val = row[self.split_col].value
             if (None != col_val):
                 if (col_val in splits):
@@ -73,7 +73,7 @@ class Vireo:
 
     def _id_rows(self):
         if not self.id_rows:
-            for row in self.sheet.iter_rows(min_row=2):
+            for row in self.thesis.iter_rows(min_row=2):
                 id = row[self.id_col].value
                 if (id in self.id_rows):
                     raise "duplicate id %s" % str(id)
@@ -92,7 +92,7 @@ class Vireo:
         print("< Print %s" % name);
 
     def _header(self):
-        return next(self.sheet.iter_rows(min_row=1, max_row=1))
+        return next(self.thesis.iter_rows(min_row=1, max_row=1))
 
     def _print_tsv_row(row, out):
         #print('\t'.join([c.value for c in row]), file=out)
@@ -104,7 +104,8 @@ def main():
     loglevels = ['CRITICAL', 'ERROR', 'WARN', 'INFO', 'DEBUG', 'NOTSET']
 
     parser = ArgParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument("--excel", "-s", default=None, required=True, help="excel export file from vireo")
+    parser.add_argument("--thesis", "-t", default=None, required=True, help="excel export file from vireo")
+    parser.add_argument("--add_certs", "-a", default=None, required=False, help="excep spreadsheet with addituion certificate program info")
     parser.add_argument("--loglevel", choices=loglevels,  default=logging.ERROR, help="log level  - default: ERROR")
     args = parser.parse_args()
     try:
